195a1f8267bff0b31073cebfec6162e11799e492
Tim Hung <thung1@binghamton.edu>
2014-12-21 08:15:54 +0000

    Everything seems functional. Post request is working, validity checking works.

--- templates/practice.js
+++ templates/practice.js
@@ -1,13 +1,35 @@
 $("input[type=submit]").click(function(){
-	// $.ajax({
-	//   type: "POST",
-	//   url: url,
-	//   data: data, 
-	//   success: success,
-	//   dataType: dataType
-	// });
-	$alert("Clicked submit!");
-	$.post("demo_test.asp",function(data,status){
-		alert("Data: " + data + "\nStatus: " + status);
+				
+	var fieldData = [];
+	
+	$('input[type=text]').each(function() {
+	    fieldData[$(this).attr('id')] = $(this).val();
 	});
+
+	var isFull = true;
+
+	for (var i in fieldData) {
+	  if(fieldData[i] == ""){isFull = false;}
+	}
+
+	if(!isFull){
+		alert("At least one of your fields is blank! Please fill out all fields.")
+	}
+	else if(!isAlphabetic(fieldData[fieldData.length - 1])){
+		alert("The last field cannot have non-alphabetic characters!")
+	}
+	else{
+		$.post('../controller/practice_actions.php', {fieldData: fieldData}, function(data){
+			//$('#testDiv').html(data);
+			alert("Your information has been saved!");
+		});
+	}
 });
+
+function isAlphabetic(str){
+    if( /[^a-zA-Z+$]/.test(str) ) {
+       //alert('Input is not alphabetic');
+       return false;
+    }
+    return true;     
+}
